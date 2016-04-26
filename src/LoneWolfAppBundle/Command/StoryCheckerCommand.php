c4861bde5cadbf15ecb5de46bb06569b8c9daef9
Florian Lanson <f.lanson@eurelis.com>
2016-04-26 21:13:55 +0200

    Add commands

---
+++ src/LoneWolfAppBundle/Command/StoryCheckerCommand.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Grumly
+ * Date: 12/04/2016
+ * Time: 22:09
+ */
+
+namespace LoneWolfAppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StoryCheckerCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('lonewolf:story:check')
+            ->setDescription('Load story stored in json')
+            ->addArgument(
+                'storyList',
+                InputArgument::OPTIONAL,
+                'List of story you want to apply ex 1:1,4,7_2:1,5_3:6,8,7'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $storyListOptionText = $input->getArgument('storyList');
+        $subCampaignList = [];
+        if (!empty($storyListOptionText)) {
+            $campaignArray = explode('_', $storyListOptionText);
+            foreach ($campaignArray as $campaignText) {
+                $campaignInfo = explode(':', $campaignText);
+                $subCampaignList[$campaignInfo[0]] = explode(',', $campaignInfo[1]);
+            }
+        }
+
+        $storyLoader = $this->getContainer()->get('lone_wolf_app.story_checker');
+        $storyLoader->compareCampaignList($subCampaignList);
+    }
+}
---
+++ src/LoneWolfAppBundle/Command/StoryCrawlerCommand.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Grumly
+ * Date: 12/04/2016
+ * Time: 22:09
+ */
+
+namespace LoneWolfAppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StoryCrawlerCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('lonewolf:story:crawl')
+            ->setDescription('Load story stored in json')
+            ->addArgument(
+                'storyList',
+                InputArgument::OPTIONAL,
+                'List of story you want to apply ex 1:1,4,7_2:1,5_3:6,8,7'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $storyListOptionText = $input->getArgument('storyList');
+        $subCampaignList = [];
+        if (!empty($storyListOptionText)) {
+            $campaignArray = explode('_', $storyListOptionText);
+            foreach ($campaignArray as $campaignText) {
+                $campaignInfo = explode(':', $campaignText);
+                $subCampaignList[$campaignInfo[0]] = explode(',', $campaignInfo[1]);
+            }
+        }
+        $storyLoader = $this->getContainer()->get('lone_wolf_app.story_crawler');
+        $storyLoader->crawlCampaignList($subCampaignList);
+    }
+}
